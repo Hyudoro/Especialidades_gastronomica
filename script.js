39d3c13427124b2c643b8d101bcae6bef5ac9efe
Jhon-Stiven <jh.virtual.tech@gmail.com>
2024-05-18 17:49:27 +0200

    refont

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
- const OpenNav = document.querySelector(".icon1")
- const CloseNav = document.querySelector(".fermer")
- const Menu = document.querySelector(".menu")
+const OpenNav = document.querySelector(".icon1");
+const CloseNav = document.querySelector(".fermer");
+const Menu = document.querySelector(".menu");
 
+OpenNav.addEventListener("click", () => {
+    Menu.classList.add("monter");
+});
 
- const PositionMenu = Menu.getBoundingClientRect().left;
-
-
-
- OpenNav.addEventListener("click",()=>{
-    if(PositionMenu<0){
-        Menu.classList.add("monter")
-    }
- })
-
- CloseNav.addEventListener("click",()=>{
-    if(PositionMenu<0){
-        Menu.classList.remove("monter")
-    }
- })
+CloseNav.addEventListener("click", () => {
+    Menu.classList.remove("monter");
+});
